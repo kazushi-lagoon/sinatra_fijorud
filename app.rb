8f0ada94ba2f21025bf4a7fa3e25648ed833ba23
kazushi-lagoon <kazu0226_7382@yahoo.co.jp>
2020-06-22 08:51:14 +0900

    make oriented_object
    オブジェクトプログラムに修正しました。機能は変わっていません。

--- app.rb
+++ app.rb
@@ -2,7 +2,48 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'fileutils'
+require 'pg'
+
+# control memo
+class Memo
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def self.db
+    PG.connect( dbname: 'memo' )
+  end
+
+  def self.set
+    Memo.db.exec("select * from memos ;").to_a
+  end
+
+  def self.memos_count
+    Memo.db.exec("select count(*) from memos ;").to_a.first["count"].to_i
+  end
+
+  def memo_order(id)
+    Memo.set.index{|i| i['id'] == Memo.db.exec("select id from memos where id = '#{id}' ;").to_a.first['id'] } + 1
+  end
+
+  def memo_content
+    Memo.db.exec("select content from memos where id = '#{number}' ;").to_a.first['content']
+  end
+
+  def content_write(content)
+    Memo.db.exec("insert into memos(content) values('#{content}') ;")
+  end
+
+  def content_edit(content)
+    Memo.db.exec("update memos set content = '#{content}' where id = '#{number}' ;")
+  end
+
+  def delete
+    Memo.db.exec("delete from memos where id = '#{number}' ;")
+  end
+end
 
 get '/new_memo' do
   erb :new_memo
@@ -18,29 +59,28 @@ post '/new' do
 end
 
 get '/' do
-  @number = Dir.open('./public/memos').children.count
-  @files = Dir.glob('./public/memos/*')
+  @memos = Memo.set
   erb :top
 end
 
 get '/edition/:i' do
   @number = params[:i]
-  @file_content = File.open("./public/memos/#{@number}.txt").read
+  @order = Memo.new(@number).memo_order(@number)
+  @file_content = Memo.new(@number).memo_content
   erb :edition
 end
 
 patch '/memo/:i' do
   number = params[:i]
   content = params[:content]
-  File.open("./public/memos/#{number}.txt", 'wb') do |f|
-    f.write(content)
-  end
+  Memo.new(number).content_edit(content)
   redirect '/'
 end
 
 get '/memo/:i' do
-  @number = params[:i]
-  @file_content = File.open("./public/memos/#{@number}.txt").read
+  @number = params[:i].to_i
+  @order = Memo.new(@number).memo_order(@number)
+  @file_content = Memo.new(@number).memo_content
   erb :memo
 end
 
