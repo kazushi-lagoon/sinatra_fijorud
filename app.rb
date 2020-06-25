8bfb5889f81abda84eb0ec5f8f223a4e318750db
kazushi-lagoon <kazu0226_7382@yahoo.co.jp>
2020-06-26 07:19:58 +0900

    make countermeasure of SQL injection, make memo_class.rb, change def self.db

--- app.rb
+++ app.rb
@@ -1,51 +1,9 @@
 # frozen_string_literal: true
 
+require_relative 'memo_class'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
-
-# control memo
-class Memo
-  attr_reader :id
-
-  def initialize(id)
-    @id = id
-  end
-
-  def self.db
-    PG.connect(dbname: 'memo')
-  end
-
-  def self.set
-    Memo.db.exec('select * from memos order by id asc;').to_a
-  end
-
-  def self.memos_count
-    Memo.db.exec('select count(*) from memos ;').to_a.first['count'].to_i
-  end
-
-  def memo_order
-    memo_id = "select id from memos where id = '#{id}' ;"
-    Memo.set.index { |i| i['id'] == Memo.db.exec(memo_id).to_a.first['id'] } + 1
-  end
-
-  def memo_content
-    memo_content = "select content from memos where id = '#{id}' ;"
-    Memo.db.exec(memo_content).to_a.first['content']
-  end
-
-  def content_write(content)
-    Memo.db.exec("insert into memos(content) values('#{content}') ;")
-  end
-
-  def content_edit(content)
-    Memo.db.exec("update memos set content = '#{content}' where id = '#{id}' ;")
-  end
-
-  def delete
-    Memo.db.exec("delete from memos where id = '#{id}' ;")
-  end
-end
 
 get '/new_memo' do
   erb :new_memo
@@ -53,8 +11,8 @@ end
 
 post '/new' do
   content = params[:content]
-  count = Memo.memos_count
-  Memo.new(count + 1).content_write(content)
+  count = Memo.count
+  Memo.new(count + 1).write_content(content)
   redirect '/'
 end
 
@@ -63,29 +21,31 @@ get '/' do
   erb :top
 end
 
-get '/edition/:i' do
-  @id = params[:i]
-  @order = Memo.new(@id).memo_order
-  @file_content = Memo.new(@id).memo_content
+get '/edition/:id' do
+  @id = params[:id]
+  memo = Memo.new(@id)
+  @order = memo.order
+  @file_content = memo.content
   erb :edition
 end
 
-patch '/memo/:i' do
-  id = params[:i]
+patch '/memo/:id' do
+  id = params[:id]
   content = params[:content]
-  Memo.new(id).content_edit(content)
+  Memo.new(id).edit_content(content)
   redirect '/'
 end
 
-get '/memo/:i' do
-  @id = params[:i]
-  @order = Memo.new(@id).memo_order
-  @file_content = Memo.new(@id).memo_content
+get '/memo/:id' do
+  @id = params[:id]
+  memo = Memo.new(@id)
+  @order = memo.order
+  @file_content = memo.content
   erb :memo
 end
 
-delete '/memo/:i' do
-  id = params[:i]
+delete '/memo/:id' do
+  id = params[:id]
   Memo.new(id).delete
   redirect '/'
 end
